f155f7ccbd4db315dda858f8d709a7939741dfb9
kharyseye <mamekharyseye380@gmail.com>
2024-01-04 16:49:56 +0000

    first commit

---
+++ src/main/java/com/groupeisi/hospital/entities/Doctor.java
@@ -0,0 +1,4 @@
+package com.groupeisi.hospital.entities;
+
+public class Doctor {
+}
---
+++ src/main/java/com/groupeisi/hospital/entities/Patient.java
@@ -0,0 +1,9 @@
+package com.groupeisi.hospital.entities;
+
+import java.util.Date;
+
+public class Patient {
+    private Long id;
+    private String name;
+    private Date birthdate;
+}
